78909dc83d6e86ea2aae715dfffc57ed5c886300
Yash Raizada <yashraizada@Yashs-MacBook-Pro.local>
2024-06-03 20:38:21 -0400

    refactor: update cik codes

--- src/core/config.py
+++ src/core/config.py
@@ -14,20 +14,20 @@ class AppConfig(BaseModel):
     # SEC Filings Downloader
     DEFAULT_STORAGE_DIR: Path = Path.cwd() / "data"
     DEFAULT_SEC_CIKS: list = [
-        "320193",        # AAPL (Apple)
+        "0000320193",    # AAPL (Apple)
         "0001018724",    # AMZN (Amazon)
         "0001730168",    # AVGO (Broadcom)
         "0000070858",    # BAC (Bank of America)
         "0001067983",    # BRK A (Berkshire Hathaway)
-        "1652044",       # GOOGL (Alphabet Inc.)
+        "0001652044",    # GOOGL (Alphabet Inc.)
         "0000200406",    # JNJ (Johnson & Johnson)
         "0000019617",    # JPM (JPMorgan Chase & Co.)
-        "59478",         # LLY (Eli Lilly)
-        "1326801",       # META (Meta Platforms)
-        "789019",        # MSFT (Microsoft)
-        "1065280",       # NFLX (Netflix)
-        "1045810",       # NVDA (NVIDIA)
-        "1318605",       # TSLA (Tesla)
+        "0000059478",    # LLY (Eli Lilly)
+        "0001326801",    # META (Meta Platforms)
+        "0000789019",    # MSFT (Microsoft)
+        "0001065280",    # NFLX (Netflix)
+        "0001045810",    # NVDA (NVIDIA)
+        "0001318605",    # TSLA (Tesla)
         "0000731766",    # UNH (UnitedHealth Group Inc.)
     ]
     DEFAULT_SEC_FILING_TYPES: list = [
